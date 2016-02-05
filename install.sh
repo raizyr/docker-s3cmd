1f792cd8e0af29e1bc051398ecf006e1a9d58d49
Chris McNabb <raizyr@gmail.com>
2016-02-05 08:06:17 -0600

    add bash for wercker ease of use

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 
 # install s3cmd
 apk update
-apk add --no-cache py-pip py-setuptools ca-certificates
+apk add --no-cache bash py-pip py-setuptools ca-certificates
 pip install python-magic
 cd /tmp
 wget https://github.com/s3tools/s3cmd/releases/download/v${S3CMD_VERSION}/s3cmd-${S3CMD_VERSION}.tar.gz
